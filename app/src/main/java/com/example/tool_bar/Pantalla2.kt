bed8c10da06521e47e4e6ec884d9219f79af2973
JoseCima <josegcima@gmail.com>
2022-03-05 20:46:53 -0500

    Configurando Botón Hacia Atras

--- app/src/main/java/com/example/tool_bar/Pantalla2.kt
+++ app/src/main/java/com/example/tool_bar/Pantalla2.kt
@@ -9,46 +9,28 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 
-
-class MainActivity : AppCompatActivity() {
+class Pantalla2 : AppCompatActivity() {
 
     var toolbar: Toolbar? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_pantalla2)
         toolbar = findViewById(R.id.toolbar)
         toolbar?.setTitle(R.string.app_name)
         //Definiendo la como oficial
         setSupportActionBar(toolbar)
 
-        val bIr  = findViewById<Button>(R.id.bIr)
-
-        bIr.setOnClickListener {
-            val intent = Intent(this, Pantalla2::class.java)
-            startActivity(intent)
-        }
+        //Creando Boton hacia atras (sin confirgurar)
+        var actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     //Creando las acciones
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
 
-        menuInflater.inflate(R.menu.menu, menu)
+        menuInflater.inflate(R.menu.menupantalla2, menu)
 
         return super.onCreateOptionsMenu(menu)
     }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
-        when(item?.itemId){
-            R.id.bFav ->{
-                Toast.makeText(this, "Elemento añadido como Favorito", Toast.LENGTH_SHORT).show()
-                return true
-            }
-            else -> {return super.onOptionsItemSelected(item)}
-        }
-
-    }
-
 
 }
